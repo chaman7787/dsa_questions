fff31c1219abbe58db1d24d457c2aa7e094f81dd
chaman7787 <jangrachaman00@gmail.com>
2026-01-29 12:39:34 +0530

    refactor: clean up commented-out code for left shift implementation

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
 
 // Shift array elements to the left by one position
-let arr = [10,20,30,40,50];
-let copy = arr[0];
+// let arr = [10,20,30,40,50];
+// let copy = arr[0];
 
-for (let i = 0; i<arr.length -1;i++){
-    arr[i] = arr[i+1];
+// for (let i = 0; i<arr.length -1;i++){
+//     arr[i] = arr[i+1];
+// }
+// arr[arr.length -1] = copy;
+
+// console.log(arr);
+
+
+// left rotation by k element
+
+let arr = [10, 20, 30, 40, 50];
+
+let k = 2;
+k = k % arr.length; // handle large k
+
+for (let j = 0; j < k; j++) {
+    let first = arr[0];
+
+    for (let i = 0; i < arr.length - 1; i++) {
+        arr[i] = arr[i + 1];
+    }
+
+    arr[arr.length - 1] = first;
 }
-arr[arr.length -1] = copy;
 
 console.log(arr);
