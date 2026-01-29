183b9b2faad7446b2a0294d242e8d0c9f08d9aa9
chaman7787 <jangrachaman00@gmail.com>
2026-01-29 12:07:58 +0530

    feat: implement left shift for array elements

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+
+// Shift array elements to the left by one position
 let arr = [10,20,30,40,50];
-length      kbdv
+let copy = arr[0];
+
+for (let i = 0; i<arr.length -1;i++){
+    arr[i] = arr[i+1];
+}
+arr[arr.length -1] = copy;
+
+console.log(arr);
