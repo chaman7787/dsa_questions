79cbbb8abcdf3904690ae5e2932337c2549eae41
chaman7787 <jangrachaman00@gmail.com>
2026-02-21 21:54:52 +0530

    feat: add prompt-sync and strip-ansi modules with basic functionality

--- index.js
+++ index.js
@@ -13,19 +13,33 @@
 
 // left rotation by k element
 
-let arr = [10, 20, 30, 40, 50];
+// let arr = [10, 20, 30, 40, 50];
 
-let k = 2;
-k = k % arr.length; // handle large k
+// let k = 2;
+// k = k % arr.length; // handle large k
 
-for (let j = 0; j < k; j++) {
-    let first = arr[0];
+// for (let j = 0; j < k; j++) {
+//     let first = arr[0];
 
-    for (let i = 0; i < arr.length - 1; i++) {
-        arr[i] = arr[i + 1];
+//     for (let i = 0; i < arr.length - 1; i++) {
+//         arr[i] = arr[i + 1];
+//     }
+
+//     arr[arr.length - 1] = first;
+// }
+
+// console.log(arr);
+
+
+// right angle triangle pattern
+
+const prompt = require("prompt-sync")();
+
+let n = parseInt(prompt("Enter the number of rows: "));
+
+for (let i=1;i<=n;i++){
+    for (let j=1;j<=i;j++){
+        process.stdout.write("* ");
     }
-
-    arr[arr.length - 1] = first;
+    console.log();
 }
-
-console.log(arr);
